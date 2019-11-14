8047e9313a1f22d5ce2212e7bd737c2014ae9a86
Zacky <zacky-kh@live.com>
2019-11-14 09:16:45 +0100

    loading component

--- src/components/ExerciseCard.vue
+++ src/components/ExerciseCard.vue
@@ -1,38 +1,36 @@
-<template>
-<div>
-
-  <div id="CardBody">
-    <img src="https://source.unsplash.com/random/400x200" class="img-fluid">
-     <!-- <img :src="getImgUrl(pic)" v-bind:alt="pic"> -->
-    <h1 class="cardTitle">{{ exercise.name }}</h1>
-    <hr class="hrStyle">
-
-    <div class="descDiv">
-        <div id="repContainer">
-            <p>Total repetitions: {{ exercise.description }}</p>
-            <p>Total repetitions: {{ exercise.imageLink }}</p>
-            <!-- <p>Total repetitions: {{ exercise.targetMuscle }}</p>
-            <p>Total repetitions: {{ exercise.videoLink }}</p> -->
-            <b-input-group v-if="!exercise.toSelectReps">
-                Reps : 
-              <b-input  id="input-1"  type="number" required v-model="reps"  placeholder="">Repetions</b-input>
-              </b-input-group>   
-               <b-input-group v-if="!exercise.toSelectSets">
-                Sets : 
-            <b-input  id="input-1"  type="number" required v-model="sets"  placeholder="">Sets</b-input>
-              </b-input-group>
-            <!-- <b-input v-if="!exercise.toSelectReps" id="input-1"  type="number" required v-model="reps"  placeholder="">Repetions</b-input> -->
-            <!-- <b-input v-if="!exercise.toSelectSets" id="input-1"  type="number" required v-model="sets"  placeholder="">Sets</b-input> -->
 
 
-        </div>
-    </div>
-    <button  v-if ="!exercise.toBeSelected"  type="submit" class="submitBtn" @click="addExerciseObj(exercise)">Select Exercise</button>
-    <!-- <button class="subminBtn" @click="addExerciseObj(exercise)">Select Exercise</button> -->
-  </div>
+ <template>
 
-</div>
-</template>
+  <b-card  
+    bg-variant="dark"
+    img-src="https://picsum.photos/600/300/?image=25"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2"
+ >
+    <b-card-text> {{ exercise.name }}</b-card-text>
+    <b-card-text> {{ exercise.description }}</b-card-text>
+
+    <b-card-text></b-card-text>
+               <b-input-group v-if="!exercise.toSelectReps">
+                 Reps : 
+               <b-input  id="input-1"  type="number" required v-model="reps"  placeholder="">Repetions</b-input>
+               </b-input-group>   
+                <b-input-group v-if="!exercise.toSelectSets">
+                 Sets : 
+             <b-input  id="input-1"  type="number" required v-model="sets"  placeholder="">Sets</b-input>
+               </b-input-group>
+     <button  v-if ="!exercise.toBeSelected"  type="submit" class="submitBtn" @click="addExerciseObj(exercise)">Select Exercise</button>
+     
+   </b-card>
+
+
+ 
+ </template> 
+
 
 <script>
 
@@ -82,49 +80,3 @@ export default {
     }
 }
 </script>
-
-<style scoped>
-
-#CardBody {
-    margin: 10px;
-    height: auto;
-    width: auto;
-    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-    transition: 0.3s;
-    background-color: rgba(20,69,79,1);
-}
-.cardTitle {
-    font-size: 25px;
-    font-weight: bold;
-    color: #fff;
-    text-align: center;
-    padding: 5px;
-}
-.descDiv {
-    height: auto;
-    width: auto;
-    padding: 5px;
-    /* background-color: green; */
-}
-#repContainer {
-    text-align: center;
-    color: #fff;
-    /* background-color: blue; */
-}
-.repBtns {
-    border: 0;
-    margin: 5px;
-    width: 50px;
-    background-color: green;
-}
-
-/* Mobile */
-@media (min-width: 360px) and (max-width: 600px) {
-
-} 
-    
-/* Tablet */
-@media (min-width: 768px) and (max-width: 1024px) {
-
-} 
-</style>
