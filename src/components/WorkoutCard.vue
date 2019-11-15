4482a5673dc82e586a4bda840ed1c99e0adad7e9
Jesper <jesper_truedsson@hotmail.com>
2019-11-15 10:44:17 +0100

    have little better code

--- src/components/WorkoutCard.vue
+++ src/components/WorkoutCard.vue
@@ -1,43 +1,39 @@
 <template>
 <div id="workoutCardContainer">
 
-    <Loading v-if="!loading"/>
+    <Loading v-if="!loading" />
 
     <div v-if="errors">
         <b-alert v-for="error in errors" :key="error" show variant="danger">{{error}}</b-alert>
     </div>
 
-
     <!-- Workout card for the regular user. -->
     <div v-if="user">
         <!-- Showing only for admin / contributors -->
-         <b-button to="/updateworkout" variant="warning" v-if="admin"> Update workout </b-button>
+        <b-button to="/updateworkout" variant="warning" v-if="admin"> Update workout </b-button>
 
-            <!-- Workout Card -->
+        <!-- Workout Card -->
         <b-card v-if="loading">
             <h1 style="text-align: center;">{{name}}</h1>
             <h5 style="text-align: center;">{{type}}</h5><br>
 
-            <!-- Div with all exercisecards -->
-            <div role="tablist" v-for="exercise in exerciseArray" :key="exercise">
-
-                 <b-card no-body class="mb-1" :exercise="exercise" v-for="id in testArray" :key="id">
-   
-                    <b-card-header header-tag="header" class="p-1" role="tab" :key="id">
-                        <b-button block v-b-toggle="'accordion-' + id" variant="info">  Exercise {{ id }} </b-button>
-                    </b-card-header>
-
-                    <b-collapse id="accordion-1" accordion="my-accordion" role="tabpanel">
-                        <Exercisecard :exercise="exercise" />
+            <b-button v-b-toggle.collapse-1 variant="primary">Toggle Collapse</b-button>
+            <b-collapse id="collapse-1" class="mt-2" v-for="exercise in exerciseArray" :key="exercise.exerciseID">
+                
+                    <p class="card-text">Collapse contents Here</p>
+                    <!-- This button needs to have a v-for from each exercisecard later.. -->
+                    <b-button v-b-toggle.collapse-1-inner size="sm" >Toggle Inner Collapse</b-button>
+                    <b-collapse id="collapse-1-inner" class="mt-2" >
+                        
+                            <Exercisecard :exercise="exercise" />
+                       
                     </b-collapse>
-              
-                 </b-card>
+                
+            </b-collapse>
 
-            </div>
         </b-card>
     </div>
 
-   
 </div>
 </template>
 
@@ -55,36 +51,33 @@ export default {
 
     data() {
         return {
-            loading : false,
+            errors: "",
+            loading: false,
             admin: false,
             user: false,
             name: "No name",
             type: "no type",
-            exerciseArray: []
+            exerciseArray: [],
+            collapseBtnsArray: []
         }
     },
 
     created() {
+        this.user = true
+        this.admin = true
+        this.loading = true
 
-        this.image = "url"
+        axios.get('https://me-fit.herokuapp.com/exercises')
+            .then(response => {
+                /* this.loading = false */
+                // JSON responses are automatically parsed.
+                this.exerciseArray.push(response.data)
 
-    this.loading = true
-    this.user = true
-    this.admin = true
-
-    this.exerciseArray.push(1,2)
-    this.testArray.push(1,2)
-
-    axios.get('http://localhost:8080/exercises')
-    .then(response => {
-        this.loading= false
-      // JSON responses are automatically parsed.
-      this.exerciseArray.push(response.data)
-      console.log(this.exerciseArray)
-    })
-    .catch(e => {
-      this.errors.push(e)
-    })
+                console.log(this.exerciseArray)
+            })
+            .catch(e => {
+                this.errors.push(e)
+            })
     },
 
     methods: {
@@ -96,7 +89,6 @@ export default {
 </script>
 
 <style scoped>
-
 /* Desktop CSS */
 #workoutCardContainer {
     margin-top: 5%;
@@ -104,7 +96,10 @@ export default {
     margin-left: 40%;
     margin-bottom: 5%;
 }
-title {text-align: center !important;}
+
+title {
+    text-align: center !important;
+}
 
 /* Mobile */
 @media (min-width: 360px) and (max-width: 600px) {
@@ -114,8 +109,8 @@ title {text-align: center !important;}
         margin-left: 10%;
         margin-bottom: 5%;
     }
-} 
-    
+}
+
 /* Tablet */
 @media (min-width: 768px) and (max-width: 1024px) {
     #workoutCardContainer {
@@ -124,5 +119,5 @@ title {text-align: center !important;}
         margin-left: 20%;
         margin-bottom: 5%;
     }
-} 
+}
 </style>
