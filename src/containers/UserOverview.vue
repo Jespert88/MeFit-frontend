4f85866f6ac065a46b556e8e340d78e57c036b6a
Zacky <zacky-kh@live.com>
2019-12-02 11:31:25 +0100

    error messages in useroverview

--- src/containers/UserOverview.vue
+++ src/containers/UserOverview.vue
@@ -89,7 +89,7 @@ export default {
         }
     },
     created: function () {
-          this.retrieveGoal
+          this.retrieveGoal()
     },
     methods: {
         retrieveGoal: function() {
